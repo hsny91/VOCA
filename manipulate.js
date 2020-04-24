cabc4f129780fce829a4955ca71caec76ae71b97
Husniye Sekeroglu <husniyesakmak.91@gmail.com>
2020-04-24 23:48:41 +0200

    add split.js

---
+++ manipulate.js
@@ -0,0 +1,43 @@
+
+
+//MANIPULATE
+
+//insert; string bit ifadenin istenilen konumuna istenilen ifadeyi ekler.
+//example.innerHTML=v.insert('voca demo','H',2);
+
+//latinise; ifadeyi latin harflerine cevirir.
+//example.innerHTML=v.latinise('cafe\u0301');
+
+//pad;
+//example.innerHTML=v.padLeft('Vocademo',15,'-');
+
+//*repeat
+//example.innerHTML=v.repeat('Voca demo',3);
+
+//*replace
+//example.innerHTML=v.replace('Vocademo','cade','xxx');
+
+//replaceAll
+//example.innerHTML=v.replaceAll('Vocademo','o','b');
+
+
+//reverse
+//example.innerHTML=v.reverse('voca demo');
+
+//splice
+//example.innerHTML=v.splice('VOCA DEMO',2,6);
+
+
+//trim
+//example.innerHTML=v.trim('   voca demo   ');
+//example.innerHTML=v.trim('---voca demo---','-');
+
+
+//wordWrap
+/*example.innerHTML=v.wordWrap('voca demo',{
+    width:4,
+    newLine:'<br>'
+
+});
+*/
+
