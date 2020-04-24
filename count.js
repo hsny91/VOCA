2eb92b50fea384e211609836826fdbc3091b7002
Husniye Sekeroglu <husniyesakmak.91@gmail.com>
2020-04-24 23:21:30 +0200

    add manipulate.js

---
+++ count.js
@@ -0,0 +1,19 @@
+//COUNT
+
+//Count;string ifadenin uzunlugunu dondurur.
+//example.innerHTML=v.count('voca demo');
+
+//countGraphemes; string ifadenin uzunlugunu dondurur.
+//example.innerHTML=v.countGraphemes('cafe\u0301');
+//console.log('cafe\u0301');
+
+
+//countSubstrings;ifadedeki belirtilen alt ifadelerin sayisini veriri.
+//example.innerHTML=v.countSubstrings('Voca demo Voca demo Voca demo','Voca');
+
+//countWhere
+//example.innerHTML=v.countWhere('Voca demo');
+
+//countWords;string ifadedeki kelime sayisini veririr.
+//example.innerHTML=v.countWords('Voca demo voca demo voca demo');
+
