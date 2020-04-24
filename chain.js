4d1c216137da12876ca67a9d9c97993d53caff1a
Husniye Sekeroglu <husniyesakmak.91@gmail.com>
2020-04-24 12:05:00 +0200

    made change

---
+++ chain.js
@@ -0,0 +1,31 @@
+//CHAIN
+
+//camelCase
+//example.innerHTML=v.camelCase('voca demo');
+
+//capitalize; string ifadenin ilk karakterini buyuk harfe cevirir.Opsiyonel olarak boolen deger alir. Eger boolen deger TRUE ise geriye kalna ifadeleri kucuk harf yapar.
+//example.innerHTML=v.capitalize('vocademo');
+//example.innerHTML=v.capitalize('vOCAdemo',true);
+
+//decapitalize;ifadenin ilk karakterini kucuk harfe cevirir
+//example.innerHTML=v.decapitalize('Vocademo');
+
+//kebabCase
+//example.innerHTML=v.kebabCase('Voca demo');
+
+//snakeCase
+//example.innerHTML=v.snakeCase('Voca demo');
+
+//v.swapCase;Buyuk harfleri kucuge kucuk harfleri buyuge cevirir.
+//example.innerHTML=v.swapCase('Voca demo');
+
+
+//titleCase; tum ifadenin sadece bas harfleri buyuk
+//example.innerHTML=v.titleCase('voca demo');
+
+//lowerCase
+//example.innerHTML=v.lowerCase('VOCA DEMO');
+
+
+//upperCase
+//example.innerHTML=v.upperCase('voca demo');
---
+++ chop.js
@@ -0,0 +1,43 @@
+//CHOP
+
+//charAt
+//example.innerHTML=v.charAt('voca demo',2);
+
+//codePointAt; unicode degeri dondurur.
+//example.innerHTML=v.codePointAt('VOCAdemo',3);
+
+//first; ifadenin ilk karakterini dondurur.Opsiyonel olarak ifadenin ilk kac karakterini yazabiliriz.
+//example.innerHTML=v.first('Vocademo');
+//example.innerHTML=v.first('Vocademo',2); 
+
+//last
+//example.innerHTML=v.last('Voca demo');
+
+//graphemeAt
+example.innerHTML=v.graphemeAt('Voca demo',3);
+
+//v.swapCase;Buyuk harfleri kucuge kucuk harfleri buyuge cevirir.
+//example.innerHTML=v.swapCase('Voca demo');
+
+
+//titleCase; tum ifadenin sadece bas harfleri buyuk
+//example.innerHTML=v.titleCase('voca demo');
+
+//lowerCase
+//example.innerHTML=v.lowerCase('VOCA DEMO');
+
+
+//upperCase
+//example.innerHTML=v.upperCase('voca demo');
+/*
+v.
+v.f
+v.graphemeAt
+v.
+v.prune
+v.slice
+v.substr
+v.substring
+v.truncate
+Count
+*/
